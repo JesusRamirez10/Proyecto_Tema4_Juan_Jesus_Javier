af95259e3531c2f7ccb0f9ea7fb40b4d54f2efbc
JesusRamirez10 <jesusramirez1627@gmail.com>
2025-12-04 19:07:48 +0100

    Estructura y modelos visitantes y atracciones

---
+++ models/atracciones_model.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from peewee import *
+from playhouse import postgres_ext
+from models.basemodel import BaseModel
+
+tipos_atraccion = (('extrema','Extrema'),('familiar','Familiar')('infantil','Infantil'),('acuatica','Acuatica'),)
+
+class AtraccionesModel(BaseModel):
+     # Peewee usa AutoField por defecto para IDs primarias autoincrementables
+    id = AutoField(primary_key = True)
+    # CharField con longitud ilimitada 
+    nombre = CharField(unique=True,null=False)
+    # tipo (string) con restricción de los 4 tipos.
+    tipo = CharField(eleccion=tipos_atraccion,null=False)
+    # altura_minima que se guarda en CM
+    altura_minima = IntegerField(null=False)
+    #detalles(jsonb) con su estructura
+    detalles = postgres_ext.BinaryJSONField(null=True,default={
+        "duracion_segundos": 0,
+        "capacidad_por_turno": 0,
+        "intensidad": 0,
+        "caracteristicas": [],
+        "horarios": {
+            "apertura": "",
+            "cierre": "",
+            "mantenimiento": []
+        }
+    })
+
+    activa = BooleanField(default=True)
+
+    fecha_inauguracion = DateField(default=datetime.now)
+
+
+
---
+++ models/visitantes_model.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from peewee import *
+from playhouse import postgres_ext
+from models.basemodel import BaseModel
+
+class VisitantesModel(BaseModel):
+    # Peewee usa AutoField por defecto para IDs primarias autoincrementables
+    id = AutoField(primary_key=True) 
+    
+    # CharField con longitud ilimitada 
+    nombre = CharField(null=False) 
+    
+    # CharField con restricción de único y no nulo
+    email = CharField(null=False, unique=True) 
+    
+    # IntegerField
+    altura = IntegerField(null=False) 
+    
+    # fecha_registro (datetime)
+    # DateTimeField con valor por defecto
+    fecha_registro = DateTimeField(default=datetime.now)
+
+    preferencias = postgres_ext.BinaryJSONField(null=True, default={
+        "tipo_favorito": "",
+        "restricciones": [],
+        "historial_visitas": []
+    })
